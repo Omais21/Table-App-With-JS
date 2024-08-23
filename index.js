c97d5b9ec6397a2a769dfce08f5a78ac30f79109
Omais Rao <154328445+OmaisRao@users.noreply.github.com>
2024-08-23 20:14:15 +0500

    ad

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nUm=prompt("Please enter a number to generate its multiplication table:")
-
+alert("Check Console Panel")
 for (let i = 1; i <= 10; i++) {
    console.log(nUm ,"x", i, "=",nUm*i)
 }
